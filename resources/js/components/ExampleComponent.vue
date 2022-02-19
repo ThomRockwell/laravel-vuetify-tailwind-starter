8189f9ef251d6067222fbc59e0534e82c1306990
Thom Rockwell <thom@rockwelldigital.com>
2022-02-19 09:48:45 -0700

    Installed and implemented Vuex. Included tester in ExampleComponent.

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -12,7 +12,8 @@
             </v-btn>
             <v-slider></v-slider>
             <v-date-picker></v-date-picker>
-            <div class="bg-yellow-400 p-8 text-4xl text-center rounded-xl">Testing Tailwind CSS...</div>
+            <div class="bg-yellow-400 p-8 text-xl text-center rounded-xl w-max mx-auto">Testing Tailwind CSS...</div>
+            <div class="bg-blue-300 w-2/12 mx-auto text-center text-xl p-4 rounded-lg mt-4 cursor-pointer" @click="testVuex">Test Vuex</div>
         </v-container>
     </v-app>
 </template>
@@ -21,6 +22,16 @@
     export default {
         mounted() {
             console.log('Component mounted.')
+        },
+
+        methods: {
+
+            testVuex(){
+                console.log("testVuex method triggered");
+                this.$store.dispatch('increment');
+            }
+
         }
+
     }
 </script>
