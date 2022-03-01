159e925ca51d1ddf5942433ce7bf2b54464041e1
Thom Rockwell <thom@rockwelldigital.com>
2022-03-01 12:35:47 -0700

    Added width and height dipslays to Breakpoints Displayer.

--- resources/js/components/global/BreakpointsDisplayer.vue
+++ resources/js/components/global/BreakpointsDisplayer.vue
@@ -1,14 +1,38 @@
 <template>
 
-    <div class="flex relative w-full bg-blue-600 mb-12">
+    <div>
 
-        <div class="displayer visible sm:invisible bg-purple-300">Extra Small (default breakpoint)</div>
-        <div class="displayer invisible sm:visible md:invisible bg-green-300">Small</div>
-        <div class="displayer invisible md:visible lg:invisible bg-blue-300">Medium</div>
-        <div class="displayer invisible lg:visible xl:invisible bg-red-300">Large</div>
-        <div class="displayer invisible xl:visible bg-indigo-300">Extra Large</div>
+        <div class="flex relative w-full bg-blue-600 mb-12">
+
+            <div class="flex displayer visible sm:invisible bg-purple-300 justify-between">
+                <div class="w-max">Extra Small (default breakpoint)</div>
+                <div class="w-max">w:{{ width }} | h: {{ height}}</div>
+            </div>
+
+            <div class="flex displayer invisible sm:visible md:invisible bg-green-300 justify-between">
+                <div class="w-max">Small</div>
+                <div class="w-max">w:{{ width }} | h: {{ height}}</div>
+            </div>
+
+            <div class="flex displayer invisible md:visible lg:invisible bg-blue-300 justify-between">
+                <div class="w-max">Medium</div>
+                <div class="w-max">w:{{ width }} | h: {{ height}}</div>
+            </div>
+
+            <div class="flex displayer invisible lg:visible xl:invisible bg-red-300 justify-between">
+                <div class="w-max">Large</div>
+                <div class="w-max">w:{{ width }} | h: {{ height}}</div>
+            </div>
+
+            <div class="flex displayer invisible xl:visible bg-indigo-300 justify-between">
+                <div class="w-max">Extra Large</div>
+                <div class="w-max">w:{{ width }} | h: {{ height}}</div>
+            </div>
+
+        </div>
 
     </div>
+
 
 </template>
 
@@ -16,7 +40,23 @@
 
     export default {
 
-        name: "BreakpointsDisplayer"
+        name: "BreakpointsDisplayer",
+
+        data() {
+            return { width: window.innerWidth, height: window.innerHeight };
+        },
+        created() {
+            window.addEventListener("resize", this.onResize);
+        },
+        destroyed() {
+            window.removeEventListener("resize", this.onResize);
+        },
+        methods: {
+            onResize(e) {
+                this.width = window.innerWidth;
+                this.height = window.innerHeight;
+            },
+        },
 
     }
 
